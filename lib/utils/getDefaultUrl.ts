43189bb6afe492e0669604d201b7b5b383bc7c1e
Ethan <ethan@Ethanui-MacBookPro.local>
2024-11-09 11:12:26 +0900

    [공통] api base url 수정

--- lib/utils/getDefaultUrl.ts
+++ lib/utils/getDefaultUrl.ts
@@ -1,5 +1,5 @@
 const getDefaultUrl = () => {
-  const host = process.env.NODE_ENV === "production" ? "url" : "localhost:3000";
+  const host = process.env.NODE_ENV === "production" ? "jake-history.vercel.app" : "localhost:3000";
   const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
 
   return `${protocol}://${host}`;
