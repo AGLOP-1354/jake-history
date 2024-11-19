2c55c83a2978ce689e5d363ac006353153304f39
Ethan <ethan@EthanuicBookPro.playio.kr>
2024-11-19 13:26:43 +0900

    [공통] 사이트맵 설정 수정 2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-  siteUrl: 'https://jake-history.vercel.app/',
+  siteUrl: 'https://jake-history.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
   priority: 0.7,
   exclude: ['/server-sitemap.xml'],
+  sitemapBaseFileName: 'sitemap',
+  xmlNs: 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"',
+  transform: (config, url) => {
+    return {
+      loc: url,
+      changefreq: 'daily',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    }
+  },
   robotsTxtOptions: {
     additionalSitemaps: [
       'https://jake-history.vercel.app/sitemap.xml',
